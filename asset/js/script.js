3acea381812747a16b2764bd1022caba70d58fcd
ruis21 <ruinoa21@gmail.com>
2025-12-08 19:05:15 +0900

    edit

--- asset/js/script.js
+++ asset/js/script.js
@@ -98,74 +98,74 @@ mm.add("(max-width: 768px)", () => {
 // merit 영역 =================================
 
 // PC (769px 이상)
-mm.add("(min-width: 769px)", () => {
-    gsap.set([".card02", ".card03", ".card04", ".card05"], { opacity: 0 });
+// mm.add("(min-width: 769px)", () => {
+//     gsap.set([".card02", ".card03", ".card04", ".card05"], { opacity: 0 });
 
-    const tl = gsap.timeline({
-        scrollTrigger: {
-            trigger: "#pinned",
-            start: "top 15%",
-            end: "+=500%",
-            pin: true,
-            scrub: 1,
-        }
-    });
+//     const tl = gsap.timeline({
+//         scrollTrigger: {
+//             trigger: "#pinned",
+//             start: "top 15%",
+//             end: "+=500%",
+//             pin: true,
+//             scrub: 1,
+//         }
+//     });
 
-    tl.to(".card01", { y: -40, opacity: 0 })
-        .fromTo(".card02", { y: 40, opacity: 0 }, { y: 0, opacity: 1 })
-        .to(".card02", { y: -40, opacity: 0 })
-        .fromTo(".card03", { y: 40, opacity: 0 }, { y: 0, opacity: 1 })
-        .to(".card03", { y: -40, opacity: 0 })
-        .fromTo(".card04", { y: 40, opacity: 0 }, { y: 0, opacity: 1 })
-        .to(".card04", { y: -40, opacity: 0 })
-        .fromTo(".card05", { y: 40, opacity: 0 }, { y: 0, opacity: 1 });
-});
+//     tl.to(".card01", { y: -40, opacity: 0 })
+//         .fromTo(".card02", { y: 40, opacity: 0 }, { y: 0, opacity: 1 })
+//         .to(".card02", { y: -40, opacity: 0 })
+//         .fromTo(".card03", { y: 40, opacity: 0 }, { y: 0, opacity: 1 })
+//         .to(".card03", { y: -40, opacity: 0 })
+//         .fromTo(".card04", { y: 40, opacity: 0 }, { y: 0, opacity: 1 })
+//         .to(".card04", { y: -40, opacity: 0 })
+//         .fromTo(".card05", { y: 40, opacity: 0 }, { y: 0, opacity: 1 });
+// });
 
-// Tablet (768~542px)
-mm.add("(max-width: 768px) and (min-width: 542px)", () => {
-    gsap.set([".card01", ".card02"], { opacity: 1 });
-    gsap.set([".card03", ".card04", ".card05"], { opacity: 0 });
+// // Tablet (768~542px)
+// mm.add("(max-width: 768px) and (min-width: 542px)", () => {
+//     gsap.set([".card01", ".card02"], { opacity: 1 });
+//     gsap.set([".card03", ".card04", ".card05"], { opacity: 0 });
 
-    const tl = gsap.timeline({
-        scrollTrigger: {
-            trigger: "#pinned",
-            start: "top 10%",
-            end: "+=600%",
-            pin: true,
-            scrub: 1,
-        }
-    });
+//     const tl = gsap.timeline({
+//         scrollTrigger: {
+//             trigger: "#pinned",
+//             start: "top 10%",
+//             end: "+=600%",
+//             pin: true,
+//             scrub: 1,
+//         }
+//     });
 
-    tl.to([".card01", ".card02"], { y: -40, opacity: 0 })
-        .fromTo([".card03", ".card04"], { y: 40, opacity: 0 }, { y: 0, opacity: 1 })
-        .to({}, { duration: 0.8 }) // 잠깐 유지
-        .to([".card03", ".card04"], { y: -40, opacity: 0 })
-        .fromTo(".card05", { y: 40, opacity: 0 }, { y: 0, opacity: 1 });
-});
+//     tl.to([".card01", ".card02"], { y: -40, opacity: 0 })
+//         .fromTo([".card03", ".card04"], { y: 40, opacity: 0 }, { y: 0, opacity: 1 })
+//         .to({}, { duration: 0.8 }) // 잠깐 유지
+//         .to([".card03", ".card04"], { y: -40, opacity: 0 })
+//         .fromTo(".card05", { y: 40, opacity: 0 }, { y: 0, opacity: 1 });
+// });
 
-// Mobile (541px 이하)
-mm.add("(max-width: 541px)", () => {
-    gsap.set([".card02", ".card03", ".card04", ".card05"], { opacity: 0 });
+// // Mobile (541px 이하)
+// mm.add("(max-width: 541px)", () => {
+//     gsap.set([".card02", ".card03", ".card04", ".card05"], { opacity: 0 });
 
-    const tl = gsap.timeline({
-        scrollTrigger: {
-            trigger: "#pinned",
-            start: "top 15%",
-            end: "+=500%",
-            pin: true,
-            scrub: 1,
-        }
-    });
+//     const tl = gsap.timeline({
+//         scrollTrigger: {
+//             trigger: "#pinned",
+//             start: "top 15%",
+//             end: "+=500%",
+//             pin: true,
+//             scrub: 1,
+//         }
+//     });
 
-    tl.to(".card01", { y: -40, opacity: 0 })
-        .fromTo(".card02", { y: 40, opacity: 0 }, { y: 0, opacity: 1 })
-        .to(".card02", { y: -40, opacity: 0 })
-        .fromTo(".card03", { y: 40, opacity: 0 }, { y: 0, opacity: 1 })
-        .to(".card03", { y: -40, opacity: 0 })
-        .fromTo(".card04", { y: 40, opacity: 0 }, { y: 0, opacity: 1 })
-        .to(".card04", { y: -40, opacity: 0 })
-        .fromTo(".card05", { y: 40, opacity: 0 }, { y: 0, opacity: 1 });
-});
+//     tl.to(".card01", { y: -40, opacity: 0 })
+//         .fromTo(".card02", { y: 40, opacity: 0 }, { y: 0, opacity: 1 })
+//         .to(".card02", { y: -40, opacity: 0 })
+//         .fromTo(".card03", { y: 40, opacity: 0 }, { y: 0, opacity: 1 })
+//         .to(".card03", { y: -40, opacity: 0 })
+//         .fromTo(".card04", { y: 40, opacity: 0 }, { y: 0, opacity: 1 })
+//         .to(".card04", { y: -40, opacity: 0 })
+//         .fromTo(".card05", { y: 40, opacity: 0 }, { y: 0, opacity: 1 });
+// });
 
 // preview Swiper ==============================================
 var previewSwiper = new Swiper(".previewSwiper", {
