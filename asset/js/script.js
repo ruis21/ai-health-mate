60b821eb6491f3039b444e6119ba9cd6863a4d28
ruis21 <ruinoa21@gmail.com>
2025-12-09 15:10:25 +0900

    history stopped

--- asset/js/script.js
+++ asset/js/script.js
@@ -286,53 +286,53 @@ tl.to(".about__text-wrap .line:nth-child(1)", { opacity: 1, y: 0, duration: 1 })
 
 // history gsap =============================
 
-let boxes = gsap.utils.toArray(".history__right"),
-    container = document.querySelector(".history__wrap"),
-    text = document.querySelector(".history__left-wrap"),
-    padding = gsap.getProperty(container, "paddingTop", "px"),
-    // create a ScrollTrigger for each box that we can use to calculate snapping (we'll look at the "start" of each in the onRefresh)
-    snapTriggers = boxes.map((box) =>
-        ScrollTrigger.create({
-            trigger: box,
-            start: "top " + padding + "px"
-        })
-    ),
-    snaps = []; // where we'll store the progress value for each box's ScrollTrigger (start)
+// let boxes = gsap.utils.toArray(".history__right"),
+//     container = document.querySelector(".history__wrap"),
+//     text = document.querySelector(".history__left-wrap"),
+//     padding = gsap.getProperty(container, "paddingTop", "px"),
+//     // create a ScrollTrigger for each box that we can use to calculate snapping (we'll look at the "start" of each in the onRefresh)
+//     snapTriggers = boxes.map((box) =>
+//         ScrollTrigger.create({
+//             trigger: box,
+//             start: "top " + padding + "px"
+//         })
+//     ),
+//     snaps = []; // where we'll store the progress value for each box's ScrollTrigger (start)
 
-ScrollTrigger.create({
-    trigger: ".history__wrap",
-    markers: true,
-    pin: ".history__left-wrap",
-    pinSpacing: false,
-    start: "top top",
-    end: () =>
-        "+=" +
-        (boxes[boxes.length - 1].getBoundingClientRect().top -
-            boxes[0].getBoundingClientRect().top),
-    onRefresh: (self) => {
-        // re-populate the "snaps" Array with the progress values for where each box hits the target spot.
-        let distance = self.end - self.start;
-        snapTriggers.forEach(
-            (trigger, i) => (snaps[i] = (trigger.start - self.start) / distance)
-        );
-    },
-    snap: snaps
-});
+// ScrollTrigger.create({
+//     trigger: ".history__wrap",
+//     markers: true,
+//     pin: ".history__left-wrap",
+//     pinSpacing: false,
+//     start: "top top",
+//     end: () =>
+//         "+=" +
+//         (boxes[boxes.length - 1].getBoundingClientRect().top -
+//             boxes[0].getBoundingClientRect().top),
+//     onRefresh: (self) => {
+//         // re-populate the "snaps" Array with the progress values for where each box hits the target spot.
+//         let distance = self.end - self.start;
+//         snapTriggers.forEach(
+//             (trigger, i) => (snaps[i] = (trigger.start - self.start) / distance)
+//         );
+//     },
+//     snap: snaps
+// });
 
-// for swapping in the text for each section
-boxes.forEach((box, i) => {
-    ScrollTrigger.create({
-        trigger: box,
-        start: "top center",
-        end: "bottom center",
-        onToggle: (self) => {
-            if (self.isActive) {
-                // you could animate this in (fade it or whatever)
-                text.innerText = "202" + (i + 1);
-            }
-        }
-    });
-});
+// // for swapping in the text for each section
+// boxes.forEach((box, i) => {
+//     ScrollTrigger.create({
+//         trigger: box,
+//         start: "top center",
+//         end: "bottom center",
+//         onToggle: (self) => {
+//             if (self.isActive) {
+//                 // you could animate this in (fade it or whatever)
+//                 text.innerText = "202" + (i + 1);
+//             }
+//         }
+//     });
+// });
 
 
 
