9998194903ded56dabf760b98caee68c717c4bca
ruis21 <ruinoa21@gmail.com>
2025-12-15 17:34:57 +0900

    edit file
    - mixin bgimg 편집
    - inquiry after 넣기
    - lineClamp mixin 만들기
    - membership 배경 이미지 조절

--- asset/js/script_contact.js
+++ asset/js/script_contact.js
@@ -33,3 +33,43 @@ $(".faq__board-title").on("click", function (e) {
     $(".faq__board-list li").removeClass("on");
     $(".faq__board-list li").eq(idx).addClass("on");
 });
+
+
+// faq__tab-menu 드래그 스크롤 기능 ======================
+const tabMenu = document.querySelector('.faq__tab-menu');
+
+if (tabMenu) {
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    tabMenu.addEventListener('mousedown', (e) => {
+        isDown = true;
+        tabMenu.style.cursor = 'grabbing';
+        startX = e.pageX - tabMenu.offsetLeft;
+        scrollLeft = tabMenu.scrollLeft;
+    });
+
+    // document에서 mouseup 감지 (tabMenu 밖에서도)
+    document.addEventListener('mouseup', () => {
+        isDown = false;
+        if (tabMenu) {
+            tabMenu.style.cursor = 'grab';
+        }
+    });
+
+    tabMenu.addEventListener('mouseleave', () => {
+        isDown = false;
+        tabMenu.style.cursor = 'grab';
+    });
+
+    tabMenu.addEventListener('mousemove', (e) => {
+        if (!isDown) return;
+        const x = e.pageX - tabMenu.offsetLeft;
+        const walk = (x - startX);
+        tabMenu.scrollLeft = scrollLeft - walk;
+    });
+
+    // 초기 커서 설정
+    tabMenu.style.cursor = 'grab';
+}
